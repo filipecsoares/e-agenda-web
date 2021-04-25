9165591db0f34a074765f6cb00ad1be807afe17e
Filipe Soares <filipecarlossoares@gmail.com>
2021-04-25 14:36:35 -0300

    add vue-cal to render the calendar

--- src/views/agenda/AgendaView.vue
+++ src/views/agenda/AgendaView.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>Visualizar agenda</div>
+  <h2 class="agenda-title">Agenda de NOME-EMPRESA</h2>
+  <vue-cal hide-view-selector :time="false" active-view="month" style="height: 350px;">
+  </vue-cal>
 </template>
 
-<script lang="ts">
-export default {
+<script>
+import VueCal from 'vue-cal';
+import 'vue-cal/dist/vuecal.css';
 
+export default {
+  components: { VueCal },
+  setup() {
+    console.log('setup AgendaView');
+  },
 };
 </script>
 
 <style scoped>
-
+.agenda-title {
+  text-align: center;
+}
 </style>
